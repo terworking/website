53418a290a13510e54b2944f56dc1e1a53337bac
Aynh <99479536+Aynh@users.noreply.github.com>
2022-05-11 05:29:37 +0800

    chore: Install unocss

---
+++ unocss.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig, presetIcons, presetWind } from 'unocss';
+
+export default defineConfig({
+  presets: [presetIcons(), presetWind()],
+  shortcuts: { 'icon-btn': 'w-6 h-6 text-gray-500 ' },
+});
