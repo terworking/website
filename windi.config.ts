03d5acd384d0c66939c18c663af9bef300aa17c6
Aynh <99479536+Aynh@users.noreply.github.com>
2022-05-11 05:30:21 +0800

    chore: Remove windicss

--- windi.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'windicss/helpers';
-
-export default defineConfig({
-  shortcuts: {
-    'icon-btn': 'w-6 h-6 text-gray-500 ',
-  },
-  plugins: [require('windicss/plugin/forms')],
-});
