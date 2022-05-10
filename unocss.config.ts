ba1234bcca00022c89d3bb0044572cf554c80e3c
Aynh <99479536+Aynh@users.noreply.github.com>
2022-05-11 06:06:14 +0800

    chore: Replace iconify with unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, presetIcons, presetWind } from 'unocss';
 
 export default defineConfig({
-  presets: [presetIcons(), presetWind()],
+  presets: [presetIcons({ warn: true }), presetWind()],
   shortcuts: { 'icon-btn': 'w-6 h-6 text-gray-500 ' },
 });
