e92068bc80c38b34aea4a73ec6d1b779813addc3
Aynh <99479536+Aynh@users.noreply.github.com>
2022-06-12 09:08:44 +0800

    feat: add scroll to top button

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -35,69 +35,70 @@ const headerStyle = computed(() => ({
 </script>
 
 <template>
-  <header
-    :style="headerStyle"
-    sticky
-    top-0
-    z-100
-    bg="body opacity-50"
-    transition-transform="300 md:500"
-    ease
-    backdrop-blur-md
-    border="b primary"
-  >
-    <div flex items-center h-full max-w-7xl mx-auto px="3 lg:6">
-      <button
-        aria-label="openAside"
-        lg:hidden
-        i-tabler-align-justified
-        text-accent
-        h-8
-        w-8
-        @click="aside.visible = true"
-      ></button>
-      <div flex="~ 1" items-center justify="center lg:start">
-        <NuxtLink to="/">
-          <div flex items-center h-8 space-x-2>
-            <TerworkingLogo h-8 />
-            <span font-bold text-2xl>Terworking</span>
-          </div>
-        </NuxtLink>
-      </div>
-      <nav hidden lg:flex items-center h-full space-x-4>
-        <template v-for="{ name, path } of routes" :key="path">
-          <NuxtLink
-            capitalize
-            font-bold
-            :class="{
-              'text-accent underline underline-dotted':
-                $route.path === path ||
-                ($route.path.startsWith(path) && path !== '/'),
-            }"
-            :to="path"
-          >
-            {{ name }}
-          </NuxtLink>
-        </template>
-      </nav>
-      <div flex="~ lg:1" items-center justify-end gap-1>
-        <template v-for="{ icon, kind, url } of social" :key="url">
-          <NuxtLink
-            text-accent
-            :aria-label="`${kind.toLowerCase()}Link`"
-            :class="icon + (kind !== 'GitHub' ? ' hidden lg:block' : '')"
-            :to="url"
-            target="_blank"
-          />
-        </template>
+  <div sticky top-0 z-100>
+    <header
+      :style="headerStyle"
+      z-100
+      bg="body opacity-50"
+      backdrop-blur-md
+      border="b primary"
+      transition-transform="300 md:500"
+      ease
+    >
+      <div flex items-center h-full max-w-7xl mx-auto px="3 lg:6">
         <button
-          aria-label="toggleColorMode"
-          i-tabler-sun
-          dark:i-tabler-moon
+          aria-label="openAside"
+          lg:hidden
+          i-tabler-align-justified
           text-accent
-          @click="toggleColorMode"
+          h-8
+          w-8
+          @click="aside.visible = true"
         ></button>
+        <div flex="~ 1" items-center justify="center lg:start">
+          <NuxtLink to="/">
+            <div flex items-center h-8 space-x-2>
+              <TerworkingLogo h-8 />
+              <span font-bold text-2xl>Terworking</span>
+            </div>
+          </NuxtLink>
+        </div>
+        <nav hidden lg:flex items-center h-full space-x-4>
+          <template v-for="{ name, path } of routes" :key="path">
+            <NuxtLink
+              capitalize
+              font-bold
+              :class="{
+                'text-accent underline underline-dotted':
+                  $route.path === path ||
+                  ($route.path.startsWith(path) && path !== '/'),
+              }"
+              :to="path"
+            >
+              {{ name }}
+            </NuxtLink>
+          </template>
+        </nav>
+        <div flex="~ lg:1" items-center justify-end gap-1>
+          <template v-for="{ icon, kind, url } of social" :key="url">
+            <NuxtLink
+              text-accent
+              :aria-label="`${kind.toLowerCase()}Link`"
+              :class="icon + (kind !== 'GitHub' ? ' hidden lg:block' : '')"
+              :to="url"
+              target="_blank"
+            />
+          </template>
+          <button
+            aria-label="toggleColorMode"
+            i-tabler-sun
+            dark:i-tabler-moon
+            text-accent
+            @click="toggleColorMode"
+          ></button>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+    <TheScrollToTop />
+  </div>
 </template>
---
+++ components/TheScrollToTop.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { defaultWindow } from '@vueuse/core';
+
+const header = useHeader();
+
+const { y: scrollY } = useScroll(defaultWindow);
+const { height } = useWindowSize();
+const show = computed(
+  () => header.value.visible && scrollY.value > height.value * 2
+);
+
+const scrollToTop = () => window.scrollTo({ behavior: 'smooth', top: 0 });
+</script>
+
+<template>
+  <Transition name="scroll-to-top">
+    <div v-if="show" fixed flex items-center justify-center my-4 w-full>
+      <div
+        inline-flex
+        space-x-2
+        p="y-2 x-4"
+        items-center
+        bg-body
+        border="~ card dashed"
+        cursor-pointer
+        select-none
+        @click="scrollToTop"
+      >
+        <span i-ci-circle-chevron-up m-0></span>
+        <span font-semibold>Scroll to top</span>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.scroll-to-top-enter-active,
+.scroll-to-top-leave-active {
+  transition-property: opacity, transform;
+  transition-timing-function: ease-in-out;
+  transition-duration: 0.2s, 0.5s;
+}
+
+.scroll-to-top-enter-from,
+.scroll-to-top-leave-to {
+  opacity: 0;
+  transform: translateY(-50%);
+}
+</style>
