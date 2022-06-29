08645ce9bc051cda050063c88bca757caf0fb122
Aynh <99479536+Aynh@users.noreply.github.com>
2022-06-29 14:52:05 +0800

    perf: always proxy `/gallery` images

--- server/routes/gallery/[...].get.ts
+++ server/routes/gallery/[...].get.ts
@@ -5,7 +5,6 @@ const gdrive = useGDrive();
 
 export default defineEventHandler(async ({ event }) => {
   const { _: path } = event.context.params as Record<string, string>;
-  const query = useQuery(event);
 
   const data = await gdrive.list(path);
   const item = data
@@ -20,29 +19,24 @@ export default defineEventHandler(async ({ event }) => {
     .replace(/[^\w.]/g, '') // remove all non-alphanumeric characters
     .replace(/\..*/, ''); // remove file extension
 
-  const { download, thumbnail } = query;
-  const downloadUrl = useImageProxy({
-    filename,
-    url,
-  });
-
-  if (download === '1') {
-    const response = await fetch(downloadUrl);
+  const proxyImage = async (url: string) => {
+    const response = await fetch(url);
     for (const [key, value] of response.headers.entries()) {
       appendHeader(event, key, value);
     }
 
     return response.body;
-  }
+  };
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const { height, width } = imageMediaMetadata!;
-
+  const { thumbnail } = useQuery(event);
   if (thumbnail !== undefined) {
     const thumbnail_ = Number.parseInt(thumbnail.toString());
     const thumbnailSize = Number.isNaN(thumbnail_)
       ? useRuntimeConfig().public.galleryThumbnailSize
       : thumbnail_;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const { height, width } = imageMediaMetadata!;
 
     const thumbnailParameter: Record<string, number> = {};
     if (thumbnailSize > 0) {
@@ -58,8 +52,13 @@ export default defineEventHandler(async ({ event }) => {
       ...thumbnailParameter,
     });
 
-    return sendRedirect(event, thumbnailUrl, 301);
+    return proxyImage(thumbnailUrl);
   } else {
-    return sendRedirect(event, downloadUrl, 301);
+    const downloadUrl = useImageProxy({
+      filename,
+      url,
+    });
+
+    return proxyImage(downloadUrl);
   }
 });
