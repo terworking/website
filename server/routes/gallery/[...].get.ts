aa5d1beaa9d7279d9e2d04ab32b19bc0d49cfe60
Aynh <99479536+Aynh@users.noreply.github.com>
2022-06-29 15:33:08 +0800

    Revert "perf: always proxy `/gallery` images"
    This reverts commit 08645ce9bc051cda050063c88bca757caf0fb122.

--- server/routes/gallery/[...].get.ts
+++ server/routes/gallery/[...].get.ts
@@ -5,6 +5,7 @@ const gdrive = useGDrive();
 
 export default defineEventHandler(async ({ event }) => {
   const { _: path } = event.context.params as Record<string, string>;
+  const query = useQuery(event);
 
   const data = await gdrive.list(path);
   const item = data
@@ -19,24 +20,29 @@ export default defineEventHandler(async ({ event }) => {
     .replace(/[^\w.]/g, '') // remove all non-alphanumeric characters
     .replace(/\..*/, ''); // remove file extension
 
-  const proxyImage = async (url: string) => {
-    const response = await fetch(url);
+  const { download, thumbnail } = query;
+  const downloadUrl = useImageProxy({
+    filename,
+    url,
+  });
+
+  if (download === '1') {
+    const response = await fetch(downloadUrl);
     for (const [key, value] of response.headers.entries()) {
       appendHeader(event, key, value);
     }
 
     return response.body;
-  };
+  }
 
-  const { thumbnail } = useQuery(event);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const { height, width } = imageMediaMetadata!;
+
   if (thumbnail !== undefined) {
     const thumbnail_ = Number.parseInt(thumbnail.toString());
     const thumbnailSize = Number.isNaN(thumbnail_)
       ? useRuntimeConfig().public.galleryThumbnailSize
       : thumbnail_;
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const { height, width } = imageMediaMetadata!;
 
     const thumbnailParameter: Record<string, number> = {};
     if (thumbnailSize > 0) {
@@ -52,13 +58,8 @@ export default defineEventHandler(async ({ event }) => {
       ...thumbnailParameter,
     });
 
-    return proxyImage(thumbnailUrl);
+    return sendRedirect(event, thumbnailUrl, 301);
   } else {
-    const downloadUrl = useImageProxy({
-      filename,
-      url,
-    });
-
-    return proxyImage(downloadUrl);
+    return sendRedirect(event, downloadUrl, 301);
   }
 });
