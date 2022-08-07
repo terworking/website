31b091bd0fc6b798f80c449db55b21cca929be78
Aynh <99479536+Aynh@users.noreply.github.com>
2022-08-07 17:18:07 +0800

    fix: escape `/ROSEMI.GIF` filename

--- components/TheHeaderMenu.vue
+++ components/TheHeaderMenu.vue
@@ -5,7 +5,7 @@
     <div
       class="absolute inset-x-0 bottom-0 h-[calc(100%-64px)] flex items-center justify-center"
     >
-      <img class="h-64 w-64" src="/ROSEMI.GIF" alt="ROSEMI" />
+      <img class="h-64 w-64" :src="'/ROSEMI.GIF'" alt="ROSEMI" />
     </div>
   </div>
 </template>
