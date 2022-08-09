1c0f2d5ede8be637e5a85c478e7e35397936c06b
Aynh <99479536+Aynh@users.noreply.github.com>
2022-08-09 14:26:55 +0800

    feat: add next and prev button to header banner

--- components/TheHeaderBanner.vue
+++ components/TheHeaderBanner.vue
@@ -1,17 +1,39 @@
 <script lang="ts" setup>
-const image =
-  'https://images.weserv.nl/?filename=ab4429adfe194dffb15f944e88e52c03&url=https%3A%2F%2F77698995-c6e9-459b-8e20-8909b7cbd84d.terworking.workers.dev%2Fab4429ad-fe19-4dff-b15f-944e88e52c03.jpg&output=webp&q=80&w=1366'
+const imageIndex = ref(0)
+const bannerSrc = computed(() => `/banner/${imageIndex.value}`)
+const bannerSizes = computed(() => `(max-width: 767px) 336px, 512px`)
+const bannerSrcSet = computed(
+  () => `${bannerSrc.value}?small 336w, ${bannerSrc.value} 512w`
+)
 </script>
 
 <template>
-  <div class="relative">
+  <div id="header-banner" class="relative overflow-hidden">
+    <div class="hidden md:block">
+      <button
+        aria-label="Prev Banner"
+        class="prev-banner-button banner-button"
+        @click="imageIndex -= 1"
+      >
+        <div class="i-material-symbols-chevron-left" />
+      </button>
+      <button
+        aria-label="Next Banner"
+        class="next-banner-button banner-button"
+        @click="imageIndex += 1"
+      >
+        <div class="i-material-symbols-chevron-right" />
+      </button>
+    </div>
     <img
       class="w-full h-84 md:h-128 object-cover filter-brightness-50 dark:filter-brightness-40"
-      :src="image"
+      :src="bannerSrc"
+      :srcset="bannerSrcSet"
+      :sizes="bannerSizes"
       alt="BANNER IMAGE"
     />
     <div
-      class="flex flex-col absolute inset-0 text-cyan-2 py-48 md:py-52 px-6 md:px-24 lg:px-36"
+      class="flex flex-col absolute inset-0 text-cyan-2 py-48 md:py-52 px-6 md:px-24 lg:px-36 pointer-events-none"
     >
       <h1 class="text-lg md:text-2xl lg:text-4xl font-semibold">
         Website TEROWROROKING
@@ -20,3 +42,37 @@ const image =
     </div>
   </div>
 </template>
+
+<style scoped>
+.banner-button {
+  --at-apply: absolute z-999 top-1/2 text-cyan-2 hover:text-cyan-1 border-3 border-current
+    rounded-full opacity-0;
+  transition: opacity 250ms linear, transform 300ms cubic-bezier(1, 0, 0, 1);
+}
+
+.banner-button > * {
+  --at-apply: w-12 h-12;
+}
+
+.prev-banner-button {
+  --at-apply: left-0;
+  transform: scale(0.1);
+}
+
+.next-banner-button {
+  --at-apply: right-0;
+  transform: scale(0.1);
+}
+
+#header-banner:hover .banner-button {
+  --at-apply: opacity-100;
+}
+
+#header-banner:hover .next-banner-button {
+  transform: translateX(-25%);
+}
+
+#header-banner:hover .prev-banner-button {
+  transform: translateX(25%);
+}
+</style>
