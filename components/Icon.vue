00e00f50c79d7623223d2043bdaed4dacf679ef3
Aynh <99479536+Aynh@users.noreply.github.com>
2022-08-18 16:22:48 +0800

    feat: change icon appearance on hover

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="h-8 w-8 md:h-10 md:w-10" />
+  <span class="icon h-8 w-8 md:h-10 md:w-10" />
 </template>
+
+<style scoped>
+.icon:hover {
+  filter: brightness(0.7);
+}
+</style>
