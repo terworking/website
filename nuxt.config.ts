7072afd70342feaadee005456ac65f9d2af1c49f
Aynh <99479536+Aynh@users.noreply.github.com>
2022-08-24 10:51:15 +0800

    feat: apply page/layout transition globally

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,13 @@ export default defineNuxtConfig({
       indexParagraph: 'Alumni TKJ SMKN 2 Kandangan (2021/2022)',
     },
   },
-  css: ['~~/assets/main.css'],
+  layoutTransition: {
+    name: 'main',
+  },
+  pageTransition: {
+    name: 'main',
+  },
+  css: ['~~/assets/main.css', '~~/assets/transition.css'],
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
