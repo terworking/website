f31d8c1a539a9f701cfbeae4333e47d6d6111e89
Aynh <99479536+Aynh@users.noreply.github.com>
2022-08-26 16:03:27 +0800

    fix: there should be a space in-between NunitoSans

--- unocss.config.ts
+++ unocss.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     presetWebFonts({
       provider: 'bunny',
       fonts: {
-        sans: 'NunitoSans',
+        sans: 'Nunito Sans',
       },
     }),
     presetIcons({
